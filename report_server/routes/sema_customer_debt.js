a0c8658a34060c5f035ea28bb1acf9955584ce3a
Kaamuli Anthony Eriya <kaminto@outlook.com>
2020-02-25 10:47:25 +0300

    fix Sequelize is not defined

--- report_server/routes/sema_customer_debt.js
+++ report_server/routes/sema_customer_debt.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Sequelize = require('sequelize');
 const semaLog = require(`${__basedir}/seama_services/sema_logger`);
 const customerDebtModal = require('../models').customer_debt;
 
