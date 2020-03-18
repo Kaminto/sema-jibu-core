4850572c66e42af5b652863fb016353704ada407
Kaamuli Anthony Eriya <kaminto@outlook.com>
2020-03-18 12:05:47 +0300

    add static files to sever

---
+++ report_server/public_react/build/precache-manifest.a506df2b05988a3585045b9aecc83ccf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51cdd823e8345d986dc28911fbb49e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa6a52c677fdac7fa0c2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f5e48d43ca485624fa6f",
+    "url": "/static/js/2.a90bf5b6.chunk.js"
+  },
+  {
+    "revision": "3daf59f4d3df1af9bda656aad7f7d826",
+    "url": "/static/js/2.a90bf5b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa6a52c677fdac7fa0c2",
+    "url": "/static/js/main.0c0d8f45.chunk.js"
+  },
+  {
+    "revision": "353adf36ece7ca31ca09",
+    "url": "/static/js/runtime-main.b68ff5f1.js"
+  }
+]);
